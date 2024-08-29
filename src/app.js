6c6c54ba9554c7abf57b299f3aec16e68b2027f4
Raphael da Silva Santos <raapphaaeell@gmail.com>
2024-08-29 18:04:34 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const session = require('express-session');
 const dotenv = require('dotenv');
-const UserController = require('./controllers/UserController'); // Importa o controlador de usuário
+const UserController = require('./controllers/UserController'); 
 
 dotenv.config();
 
 const port = 3000;
 const app = express();
 
+app.use(cors({
+    origin: '*',  // Ou especifique a origem do frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeçalhos permitidos
+}));
 app.use(session({
     secret: process.env.SESSION_SECRET || 'mysecret',
     resave: false,
@@ -22,8 +28,9 @@ app.get("/", (req, res) => {
     res.send("Página inicial");
 });
 
-app.post('/login', UserController.login); // Usando o método de login do controlador
+app.post('/login', UserController.login); 
+app.post('/register', UserController.register); 
 
 app.listen(port, () => {
-    console.log(`Servidor iniciado na porta: ${port}`);
+    console.log(`Servidor iniciado na porta: ${port}`); // Corrigi a sintaxe da template string
 });
